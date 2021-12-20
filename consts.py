d9c9375ad8a0e36a7dac8d0f8de0058b5194c604
yhetman <juliagetman5@gmail.com>
2021-12-20 09:34:05 +0200

    |commit|

--- consts.py
+++ consts.py
@@ -13,16 +13,13 @@
 
 DEBUG = True
 
-A = 1
-
-B = 0x6EE3CEEB230811759F20518A0930F1A4315A827DAC
-
-n = 0x3FFFFFFFFFFFFFFFFFFFFFB12EBCC7D7F29FF7701F
-
-m = 167
+A=1
+B=0x4A6E0856526436F2F88DD07A341E32D04184572BEB710
+n=0x3FFFFFFFFFFFFFFFFFFFFFFB981960435FE5AB64236EF
+m = 179
 
 prim_eleme = (1 << m) - 1 # ptimitive element
 
-polinominal = (1 << m) + (1 << 6) + 1 # primitive polynomial
+polinominal = (1 << m) + (1 << 4) + (1 << 2) + (1 << 1) + 1 # primitive polynomial
 
 O = (0, 0)
